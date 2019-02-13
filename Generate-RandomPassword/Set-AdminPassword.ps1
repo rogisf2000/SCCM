e808caa32c9616149f8f9f266fb917e60279aee4
Igor Finkel <rogisf2000@gmail.com>
2019-02-13 11:31:08 -0800

    Add files via upload

---
+++ Generate-RandomPassword/Set-AdminPassword.ps1
@@ -0,0 +1,27 @@
+﻿Function GET-Temppassword() {
+
+Param(
+
+[int]$length=10,
+
+[string[]]$sourcedata
+
+)
+
+ 
+
+For ($loop=1; $loop –le $length; $loop++) {
+
+            $TempPassword+=($sourcedata | GET-RANDOM)
+
+            }
+
+return $TempPassword
+
+}
+
+$ascii=$NULL;For ($a=33;$a –le 126;$a++) {$ascii+=,[char][byte]$a }
+
+$tsenv = New-Object -COMObject Microsoft.SMS.TSEnvironment
+
+$tsenv.Value("PasswordString") = GET-Temppassword –length 16 –sourcedata $ascii
